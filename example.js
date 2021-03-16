e3e4e319495702cc34eaf1c67b4ad5dd76a5e51b
roozbehR <61944934+roozbehR@users.noreply.github.com>
2021-03-15 23:42:28 -0400

    Adding an example js file using the library

---
+++ example.js
@@ -0,0 +1,31 @@
+/* JS Library usage examples */
+"use strict";
+
+
+// Circle Generator
+makeAQuiz({
+    elementId: '#myQuiz',
+    questionsArr: [
+        {
+            title: "What is your name?",
+            options: [
+                "Jack",
+                "Jhon",
+                "Jane",
+                "Michael"
+            ],
+            answer: "Michael"
+        },
+        {
+            title: "kjasd",
+            options: [
+                "Jacrewtgk",
+                "Jhoreten",
+                "Janrete",
+                "Michael"
+            ],
+            answer: "Michael"
+        }
+    ],
+    time: '1:26'
+});
